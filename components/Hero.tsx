428bd7e40c7e5d98cfe03dbc6ccd1d7213e113ea
Bruno Prestes <brunoasprestes@gmail.com>
2024-05-04 11:30:08 -0300

    ajustes fotos do hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import Image from "next/image";
 
-import bgHero from "/public/emt.jpg";
+import bgHero1 from "/public/assets/bgHero_1.png";
+import bgHero2 from "/public/assets/bgHero_2.png";
+import bgHero3 from "/public/assets/bgHero_3.png";
 import Button from "./Button";
 
 const Hero = () => {
   return (
-    <section className="max-container padding-container flex flex-col gap-5 py-10 md:gap-16 lg:py-14 xl:flex-row w-full h-screen bg-gradient-to-r from-green-500 to-green-800">
-      <div className="z-20 flex flex-1 flex-row w-full">
+    <section className="max-container padding-container flex flex-col gap-5 py-10 md:gap-16 lg:py-0 md:flex-row w-full h-screen bg-gradient-to-r from-green-500 to-green-800">
+      <div className="z-40 flex flex-1 flex-row w-full items-center">
         <div className="flex flex-col">
-          <h1 className="bold-32 lg:bold-64">
+          <h1 className="bold-32 lg:bold-52">
             Estimulação Magnética Transcraniana
           </h1>
-          <p className="regular-16 lg:regular-20 mt-5 text-green-200 xl:max-w-[620px]">
+          <p className="regular-16 lg:regular-18 mt-5 text-green-200 xl:max-w-[620px]">
             No Instituto Amparo, oferecemos uma abordagem inovadora e não
             invasiva para tratar a depressão e a ansiedade. Com a EMT,
             estimulamos áreas específicas do cérebro com pulsos magnéticos
@@ -33,7 +35,7 @@ const Hero = () => {
               ))}
             <p className="bold-16 lg:bold-20 text-green-90">
               100+
-              <span className="regular-16 lg:regular-20 ml-1">
+              <span className="regular-14 lg:regular-16 ml-1">
                 Excelentes avaliações no Google
               </span>
             </p>
@@ -46,11 +48,27 @@ const Hero = () => {
             />
           </div>
         </div>
-        <div className="rounded-xl">
+      </div>
+      <div className="h-full rounded-xl flex flex-row items-end">
+        <div className="w-auto">
           <Image
-            src={bgHero}
-            alt="Dra. Camila"
-            className="w-44 h-64 lg:w-[400px] lg:h-[auto] rounded-xl"
+            src={bgHero3}
+            alt="Dra Camila"
+            className="w-44 h-64 lg:w-[700px] lg:h-[500px] rounded-xl z-20 opacity-30"
+          />
+        </div>
+        <div className="w-auto">
+          <Image
+            src={bgHero1}
+            alt="Dra Camila"
+            className="w-44 h-64 lg:w-[700px] lg:h-[500px] rounded-xl z-30"
+          />
+        </div>
+        <div className="w-auto">
+          <Image
+            src={bgHero2}
+            alt="Dra Camila"
+            className="w-44 h-64 lg:w-[700px] lg:h-[500px] rounded-xl z-20 opacity-30"
           />
         </div>
       </div>
